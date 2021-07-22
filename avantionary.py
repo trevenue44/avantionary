c5ad993549063acd3862bbad273a76e4dd436cb6
trevennue <trevepaul2@gmail.com>
2021-07-22 19:07:57 +0000

    some minor errors corrected

--- avantionary.py
+++ avantionary.py
@@ -2,6 +2,8 @@ from avantionary_database import DataBase as db
 import avantionary_bot
 import avantionary_api
 
+# run with this not commented out to run the api
 # avantionary_api.start_api()
 
+#run with this not commented out to run the bot
 avantionary_bot.start_polling()
